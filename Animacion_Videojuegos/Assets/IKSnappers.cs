152e3572db25eadbf2bec67161481d8427a84a88
Juan Fernando Lopez <gotensferxl@gmail.com>
2023-05-20 03:12:05 -0500

    PorfinHpta

--- Animacion_Videojuegos/Assets/IKSnappers.cs
+++ Animacion_Videojuegos/Assets/IKSnappers.cs
@@ -1,21 +1,20 @@
-using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+using System.Collections;
 
-public class IKSnapper : MonoBehaviour
+public class IKSnappers : MonoBehaviour
 {
-    private float proceduralInfluence;
+    [SerializeField][Range(0, 1)] private float proceduralInfluence;
     [SerializeField] private MultiParentConstraint[] animatedBones;
     [SerializeField] private MultiParentConstraint[] proceduralBones;
     [SerializeField] private AnimationCurve activationAnimation;
     [SerializeField] private AnimationCurve deactivationAnimation;
+
     private bool currentOverride;
-    
     private void UpdateInfluence(float weight)
     {
         if (animatedBones == null) return;
-        
+
         foreach (MultiParentConstraint multiParentConstraint in animatedBones)
         {
             if (multiParentConstraint == null) continue;
@@ -23,7 +22,7 @@ public class IKSnapper : MonoBehaviour
         }
 
         if (proceduralBones == null) return;
-        
+
         foreach (MultiParentConstraint proceduralConstraint in proceduralBones)
         {
             if (proceduralConstraint == null) continue;
@@ -31,23 +30,29 @@ public class IKSnapper : MonoBehaviour
         }
     }
 
+    private void OnValidate()
+    {
+        UpdateInfluence(proceduralInfluence);
+    }
+
     public void OverrideIK(bool state)
     {
-        proceduralInfluence = state ? 1 : 0;
         if (state != currentOverride)
         {
             currentOverride = state;
             StartCoroutine(AnimateInfluence());
         }
     }
-
     IEnumerator AnimateInfluence()
     {
-        AnimationCurve curve = currentOverride? activationAnimation: deactivationAnimation;
-        //Actualiza la influencia poco a poco, hasta llegar a los puntos limite
+        //actualizacion las influencias
+
+        AnimationCurve curve = currentOverride ? activationAnimation : deactivationAnimation;
         for (float time = 0; time < 1f; time += Time.deltaTime)
         {
             proceduralInfluence = curve.Evaluate(time);
+            UpdateInfluence(proceduralInfluence);
+
             yield return null;
         }
     }
