b3959c99a4b7ba651ff6f438e0d284b1ed947c7a
Juan Fernando Lopez <gotensferxl@gmail.com>
2023-05-19 23:22:44 -0500

    Leonardo Finalizado

--- Animacion_Videojuegos/Assets/IKSnappers.cs
+++ Animacion_Videojuegos/Assets/IKSnappers.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+<<<<<<< Updated upstream
 
 public class IKSnappers : MonoBehaviour
 {
@@ -11,6 +12,23 @@ public class IKSnappers : MonoBehaviour
     {
         if (animatedBones == null) return;
         
+=======
+using System.Collections;
+
+public class IKSnappers : MonoBehaviour
+{
+    [SerializeField][Range(0, 1)] private float proceduralInfluence;
+    [SerializeField] private MultiParentConstraint[] animatedBones;
+    [SerializeField] private MultiParentConstraint[] proceduralBones;
+    [SerializeField] private AnimationCurve activationAnimation;
+    [SerializeField] private AnimationCurve deactivationAnimation;
+
+    private bool currentOverride;
+    private void UpdateInfluence(float weight)
+    {
+        if (animatedBones == null) return;
+
+>>>>>>> Stashed changes
         foreach (MultiParentConstraint multiParentConstraint in animatedBones)
         {
             if (multiParentConstraint == null) continue;
@@ -18,16 +36,49 @@ public class IKSnappers : MonoBehaviour
         }
 
         if (proceduralBones == null) return;
+<<<<<<< Updated upstream
         
+=======
+
+>>>>>>> Stashed changes
         foreach (MultiParentConstraint proceduralConstraint in proceduralBones)
         {
             if (proceduralConstraint == null) continue;
             proceduralConstraint.weight = 1 - weight;
         }
     }
+<<<<<<< Updated upstream
     
+=======
+
+>>>>>>> Stashed changes
     private void OnValidate()
     {
         UpdateInfluence(proceduralInfluence);
     }
+<<<<<<< Updated upstream
+=======
+
+    public void OverrideIK(bool state)
+    {
+        if (state != currentOverride)
+        {
+            currentOverride = state;
+            StartCoroutine(AnimateInfluence());
+        }
+    }
+    IEnumerator AnimateInfluence()
+    {
+        //actualizacion las influencias
+
+        AnimationCurve curve = currentOverride ? activationAnimation : deactivationAnimation;
+        for (float time = 0; time < 1f; time += Time.deltaTime)
+        {
+            proceduralInfluence = curve.Evaluate(time);
+            UpdateInfluence(proceduralInfluence);
+
+            yield return null;
+        }
+    }
+>>>>>>> Stashed changes
 }
